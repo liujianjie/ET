d3347ae5ff5ab541d61b9034272e64a5f72ae846
liujianjie <liujianjie@galasports.com>
2026-02-03 19:57:14 +0800

    注释掉之前获取服务器时间的UI

--- Packages/cn.etetet.lockstep/Scripts/HotfixView/Client/UI/UILSServerTime/LoginFinish_CreateUILSServerTime.cs
+++ Packages/cn.etetet.lockstep/Scripts/HotfixView/Client/UI/UILSServerTime/LoginFinish_CreateUILSServerTime.cs
@@ -1,11 +1,11 @@
 ﻿namespace ET.Client
 {
-	[Event(SceneType.LockStep)]
-	public class LoginFinish_CreateUILSServerTime: AEvent<Scene, LoginFinish>
-	{
-		protected override async ETTask Run(Scene scene, LoginFinish args)
-		{
-			await UIHelper.Create(scene, UIType.UILSServerTime, UILayer.Mid);
-		}
-	}
+	// [Event(SceneType.LockStep)]
+	// public class LoginFinish_CreateUILSServerTime: AEvent<Scene, LoginFinish>
+	// {
+	// 	protected override async ETTask Run(Scene scene, LoginFinish args)
+	// 	{
+	// 		await UIHelper.Create(scene, UIType.UILSServerTime, UILayer.Mid);
+	// 	}
+	// }
 }
